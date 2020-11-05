721a8af56b52be1dfa14c2e42b50c9ed1f55674d
Travis Bell <me@travisbell.com>
2020-11-05 16:21:41 -0700

    Re-factor a lot of how SCRAM authentication works for performance reasons.

--- lib/resty/moongoo/auth/scram.lua
+++ lib/resty/moongoo/auth/scram.lua
@@ -1,24 +1,42 @@
-local Hi = require("resty.moongoo.utils").pbkdf2_hmac_sha1
+local cbson = require("cbson")
+local pbkdf2 = require "resty.nettle.pbkdf2"
+local hmac = require "resty.nettle.hmac"
+local sha1 = require "resty.nettle.sha1"
+local base64 = require "resty.nettle.base64"
 local saslprep = require("resty.moongoo.utils").saslprep
 local pass_digest = require("resty.moongoo.utils").pass_digest
-local xor_bytestr = require("resty.moongoo.utils").xor_bytestr
 
-local b64 = ngx and ngx.encode_base64 or require("mime").b64
-local unb64 = ngx and ngx.decode_base64 or require("mime").unb64
+local char = string.char
+local byte = string.byte
+local gmatch = string.gmatch
+local sub = string.sub
+local random = math.random
 
-local hmac_sha1 = ngx and ngx.hmac_sha1 or function(str, key) return require("crypto").hmac.digest("sha1", key, str, true) end
-local sha1_bin = ngx and ngx.sha1_bin or function(str) return require("crypto").digest("sha1", str, true) end
+local function generate_sha1(key)
+  local sha1 = sha1.new()
+  sha1:update(key)
+  return sha1:digest()
+end
 
-local cbson = require("cbson")
+local function generate_hmac_sha1(key, string)
+  local hmac_sha1 = hmac.sha1.new(key)
+  hmac_sha1:update(string)
+  return hmac_sha1:digest()
+end
 
+local function xor_bytestr(a, b)
+  local res = ""
+  for i=1,#a do
+    res = res .. char(bit.bxor(byte(a,i,i), byte(b, i, i)))
+  end
+  return res
+end
 
 local function auth(db, username, password)
   local username = saslprep(username)
-  local c_nonce = b64(string.sub(tostring(math.random()), 3 , 14))
-
+  local c_nonce = base64.encode(sub(tostring(random()), 3 , 14))
   local first_bare = "n="  .. username .. ",r="  .. c_nonce
-
-  local sasl_start_payload = b64("n,," .. first_bare)
+  local sasl_start_payload = base64.encode("n,," .. first_bare)
 
   local r, err
   r, err = db:_cmd("saslStart", {
@@ -31,12 +49,10 @@ local function auth(db, username, password)
     return nil, err
   end
 
-    
   local conversationId = r['conversationId']
   local server_first = r['payload']:raw()
-
   local parsed_t = {}
-  for k, v in string.gmatch(server_first, "(%w+)=([^,]*)") do
+  for k, v in gmatch(server_first, "(%w+)=([^,]*)") do
     parsed_t[k] = v
   end
 
@@ -44,25 +60,21 @@ local function auth(db, username, password)
   local salt = parsed_t['s']
   local s_nonce = parsed_t['r']
 
-  if not string.sub(s_nonce, 1, 12) == c_nonce then
+  if not sub(s_nonce, 1, 12) == c_nonce then
     return nil, 'Server returned an invalid nonce.'
   end
 
   local without_proof = "c=biws,r=" .. s_nonce
-
-  local pbkdf2_key = pass_digest ( username , password )
-  local salted_pass = Hi(pbkdf2_key, iterations, unb64(salt), 20)
-
-  local client_key = hmac_sha1(salted_pass, "Client Key")
-  local stored_key = sha1_bin(client_key)
+  local salted_pass = pbkdf2.hmac_sha1(pass_digest(username, password), iterations, base64.decode(salt), 20)
+  local client_key = generate_hmac_sha1(salted_pass, "Client Key")
   local auth_msg = first_bare .. ',' .. server_first .. ',' .. without_proof
-  local client_sig = hmac_sha1(stored_key, auth_msg)
+  local client_sig = generate_hmac_sha1(generate_sha1(client_key), auth_msg)
   local client_key_xor_sig = xor_bytestr(client_key, client_sig)
-  local client_proof = "p=" .. b64(client_key_xor_sig)
-  local client_final = b64(without_proof .. ',' .. client_proof)
-  local server_key = hmac_sha1(salted_pass, "Server Key")
-  local server_sig = b64(hmac_sha1(server_key, auth_msg))
-    
+  local client_proof = "p=" .. base64.encode(client_key_xor_sig)
+  local client_final = base64.encode(without_proof .. ',' .. client_proof)
+  local server_key = generate_hmac_sha1(salted_pass, "Server Key")
+  local server_sig = base64.encode(generate_hmac_sha1(server_key, auth_msg))
+
   r, err = db:_cmd("saslContinue",{
     conversationId = conversationId ;
     payload =  cbson.binary(client_final);
@@ -74,13 +86,13 @@ local function auth(db, username, password)
 
   local parsed_s = r['payload']:raw()
   parsed_t = {}
-  for k, v in string.gmatch(parsed_s, "(%w+)=([^,]*)") do
+  for k, v in gmatch(parsed_s, "(%w+)=([^,]*)") do
     parsed_t[k] = v
   end
   if parsed_t['v'] ~= server_sig then
     return nil, "Server returned an invalid signature."
   end
-    
+
   if not r['done'] then
     r, err = db:_cmd("saslContinue", {
       conversationId = conversationId ;
--- lib/resty/moongoo/utils.lua
+++ lib/resty/moongoo/utils.lua
@@ -1,9 +1,23 @@
+local nmd5 = require "resty.nettle.md5"
 local bit = require("bit")
 local cbson = require("cbson")
+local hasposix, posix = pcall(require, "posix")
 
-local md5 = ngx and ngx.md5 or function(str) return require("crypto").digest("md5", str) end
-local hmac_sha1 = ngx and ngx.hmac_sha1 or function(str, key) return require("crypto").hmac.digest("sha1", key, str, true) end
-local hasposix , posix = pcall(require, "posix")
+local byte = string.byte
+local gsub = string.gsub
+local format = string.format
+
+local function hex(str, spacer)
+  return (gsub(str,"(.)", function (c)
+    return format("%02x%s", byte(c), spacer or "")
+  end))
+end
+
+local function generate_md5(string)
+  local new_md5 = nmd5.new()
+  new_md5:update(string)
+  return new_md5:digest()
+end
 
 local machineid
 if hasposix then
@@ -11,7 +25,7 @@ if hasposix then
 else
   machineid = assert(io.popen("uname -n")):read("*l")
 end
-machineid = md5(machineid):sub(1, 6)
+machineid = hex(generate_md5(machineid)):sub(1, 6)
 
 local function uint_to_hex(num, len, be)
   local len = len or 4
@@ -20,7 +34,7 @@ local function uint_to_hex(num, len, be)
   local raw = cbson.uint_to_raw(num, len, be)
   local out = ''
   for i = 1, #raw do
-    out = out .. string.format("%02x", raw:byte(i,i))
+    out = out .. format("%02x", raw:byte(i,i))
   end
   return out
 end
@@ -51,25 +65,10 @@ local function generate_oid()
   end
 
   pid = uint_to_hex(pid,2)
-
   counter = counter + 1
   local time = os.time()
 
   return uint_to_hex(time, 4, 1) .. machineid .. pid .. uint_to_hex(counter, 4, 1):sub(3,8)
-end
-
-local function print_r(t, indent)
-  local indent=indent or ''
-  if #indent > 5 then return end
-  if type(t) ~= "table" then
-    print(t)
-    return
-  end
-  for key,value in pairs(t) do
-    io.write(indent,'[',tostring(key),']')
-    if type(value)=="table" then io.write(':\n') print_r(value,indent..'\t')
-    else io.write(' = ',tostring(value),'\n') end
-  end
 end
 
 local function parse_uri(url)
@@ -78,44 +77,44 @@ local function parse_uri(url)
   -- empty url is parsed to nil
   if not url or url == "" then return nil, "invalid url" end
   -- remove whitespace
-  url = string.gsub(url, "%s", "")
+  url = gsub(url, "%s", "")
   -- get fragment
-  url = string.gsub(url, "#(.*)$", function(f)
+  url = gsub(url, "#(.*)$", function(f)
     parsed.fragment = f
     return ""
   end)
   -- get scheme
-  url = string.gsub(url, "^([%w][%w%+%-%.]*)%:", function(s) parsed.scheme = s; return "" end)
+  url = gsub(url, "^([%w][%w%+%-%.]*)%:", function(s) parsed.scheme = s; return "" end)
 
   -- get authority
   local location
-  url = string.gsub(url, "^//([^/]*)", function(n)
+  url = gsub(url, "^//([^/]*)", function(n)
     location = n
     return ""
   end)
 
   -- get query stringing
-  url = string.gsub(url, "%?(.*)", function(q)
+  url = gsub(url, "%?(.*)", function(q)
     parsed.query_string = q
     return ""
   end)
   -- get params
-  url = string.gsub(url, "%;(.*)", function(p)
+  url = gsub(url, "%;(.*)", function(p)
     parsed.params = p
     return ""
   end)
   -- path is whatever was left
-  if url ~= "" then parsed.database = string.gsub(url,"^/([^/]*).*","%1") end
+  if url ~= "" then parsed.database = gsub(url,"^/([^/]*).*","%1") end
   if not parsed.database or #parsed.database == 0 then parsed.database = "admin" end
 
   if not location then return parsed end
 
-  location = string.gsub(location,"^([^@]*)@", function(u) parsed.userinfo = u; return "" end)
+  location = gsub(location,"^([^@]*)@", function(u) parsed.userinfo = u; return "" end)
 
   parsed.hosts = {}
-  string.gsub(location, "([^,]+)", function(u)
+  gsub(location, "([^,]+)", function(u)
     local pr = { host = "localhost", port = 27017 }
-    u = string.gsub(u, ":([^:]*)$",
+    u = gsub(u, ":([^:]*)$",
       function(p) pr.port = p; return "" end)
     if u ~= "" then pr.host = u end
    table.insert(parsed.hosts, pr)
@@ -124,64 +123,30 @@ local function parse_uri(url)
 
   parsed.query = {}
   if parsed.query_string then
-    string.gsub(parsed.query_string, "([^&]+)", function(u)
-      u = string.gsub(u, "([^=]*)=([^=]*)$", function(k,v) parsed.query[k] = v; return "" end)
+    gsub(parsed.query_string, "([^&]+)", function(u)
+      u = gsub(u, "([^=]*)=([^=]*)$", function(k,v) parsed.query[k] = v; return "" end)
     end)
   end
 
   local userinfo = parsed.userinfo
   if not userinfo then return parsed end
-  userinfo = string.gsub(userinfo, ":([^:]*)$",
+  userinfo = gsub(userinfo, ":([^:]*)$",
     function(p) parsed.password = p; return "" end)
   parsed.user = userinfo
   return parsed
 end
 
-local function xor_bytestr( a, b )
-  local res = ""
-  for i=1,#a do
-    res = res .. string.char(bit.bxor(string.byte(a,i,i), string.byte(b, i, i)))
-  end
-  return res
-end
-
-local function xor_bytestr( a, b )
-  local res = ""
-  for i=1,#a do
-    res = res .. string.char(bit.bxor(string.byte(a,i,i), string.byte(b, i, i)))
-  end
-  return res
-end
-
--- A simple implementation of PBKDF2_HMAC_SHA1
-local function pbkdf2_hmac_sha1( pbkdf2_key, iterations, salt, len )
-  local u1 = hmac_sha1(pbkdf2_key, salt .. "\0\0\0\1")
-  local ui = u1
-  for i=1,iterations-1 do
-    u1 = hmac_sha1(pbkdf2_key, u1)
-    ui = xor_bytestr(ui, u1)
-  end
-  if #ui < len then
-    for i=1,len-(#ui) do
-      ui = string.char(0) .. ui
-    end
-  end
-  return ui
-end
-
 -- not full implementation, but oh well
 local function saslprep(username)
-  return string.gsub(string.gsub(username, '=', '=3D'), ',' , '=2C')
+  return gsub(gsub(username, '=', '=3D'), ',', '=2C')
 end
 
-local function pass_digest ( username , password )
-  return md5(username .. ":mongo:" .. password)
+local function pass_digest(username, password)
+  return hex(generate_md5(username .. ":mongo:" .. password))
 end
 
 return {
   parse_uri = parse_uri;
-  print_r = print_r;
-  pbkdf2_hmac_sha1 = pbkdf2_hmac_sha1;
   saslprep = saslprep;
   pass_digest = pass_digest;
   xor_bytestr = xor_bytestr;
